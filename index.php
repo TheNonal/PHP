35ae35f8dd9e2fb38849c262f6cd34f20c3f8383
TheNonal <81806620+TheNonal@users.noreply.github.com>
2023-04-10 11:30:11 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,920 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo 'IKIGAi';
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 3';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $num = 123;
+        echo $num;
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+        $a = 10;
+        echo $a;
+        echo '<br/>';
+        $a = 20;
+        echo $a;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 4';
+        echo '<br/>';
+        $a = 1 + 2 + 3;
+        echo $a;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 5';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $a = 10;
+        $b = 2;
+
+        echo 'Сумма = ', $a + $b;
+        echo '<br/>';
+        echo 'Разность = ', $a - $b;
+        echo '<br/>';
+        echo 'Произведение = ', $a * $b;
+        echo '<br/>';
+        echo 'Частное = ', $a / $b;
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $c = 10;
+        $d = 5;
+        $result = $c + $d;
+        echo 'result = ', $result;
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $a = 1;
+        $b = 2;
+        $c = 3;
+        echo 'Сумма = ', $a + $b + $c;
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $a = 10;
+        $b = 5;
+        $c = $a - $b;
+        $d = 7;
+        $result = $c + $d;
+        echo 'result = ', $result;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 7';
+        echo '<br/>';
+
+        $a = 1.5;
+        $b = 0.75;
+        echo $a + $b;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 8';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $a = -100;
+        echo $a;
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $a = 5;
+        echo -$a;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+        echo 'Razdel 9';
+        echo '<br/>';
+
+        $a = 10;
+        $b = 3;
+        echo $a % $b;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+        echo 'Razdel 10';
+        echo '<br/>';
+
+        $a = 2;
+        echo $a**10;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+        echo 'Razdel 11';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $str = 'hello';
+        echo $str;
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $str = 'abc';
+        $str2 = 'def';
+        echo $str . $str2;
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $str = 'hello';
+        $str2 = 'world';
+        echo $str . ' ' . $str2;
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $str = 'hello';
+        echo strlen($str);
+        echo '<br/>';
+
+        echo 'Zad 5';
+        echo '<br/>';
+
+        $str = 'Привет';
+        echo mb_strlen($str);
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+        echo 'Razdel 12';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        echo '<i>курсив</i>';
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        echo 1 .'<br/>'. 2 .'<br/>'. 3 .'<br/>'. 4 .'<br/>'. 5 .'<br/>';
+        echo 6 .'<br/>'. 7 .'<br/>'. 8 .'<br/>'. 9 .'<br/>';
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        echo '<img src="https://static.tildacdn.com/tild3762-3361-4530-b338-636461613366/1663115153_50-mykale.jpg" width=100 height=100>';
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        echo '<input type="text" placeholder="AKULA">';
+        echo '<br/>';
+
+        echo 'Zad 5';
+        echo '<br/>';
+
+        echo '<textarea rows="10" cols="45" name="text"></textarea>';  //СМОТРИ СЮДА!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 13';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $isAdult = true;
+        var_dump($isAdult);
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $isAdult = false;
+        var_dump($isAdult);
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 14';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $test = null;
+        var_dump($test);
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        var_dump($test2);
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 16';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $test = '12345';
+        var_dump((int) $test);
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $test = '12.345';
+        var_dump((float) $test);
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $test = '12.345';
+        var_dump((int) $test);
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $test = 12345;
+        var_dump((string) $test);
+        echo '<br/>';
+
+        echo 'Zad 5';
+        echo '<br/>';
+
+        $test = 12.345;
+        var_dump((string) $test);
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+        echo 'Razdel 17';
+        echo '<br/>';
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $str = 'abcde';
+        echo $str[0] . $str[2] . $str[4];
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $str = 'abcde';
+        echo $str[4] . $str[3] . $str[2] . $str[1] . $str[0] ;
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $str = 'abcde';
+        $num = 3;
+        echo $str[$num];
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $str = 'abcde';
+        echo $str[strlen($str) - 1];
+        echo '<br/>';
+
+        echo 'Zad 5';
+        echo '<br/>';
+
+        $str = 'abcde';
+        echo $str[strlen($str) - 2];
+        echo '<br/>';
+
+        echo 'Zad 6';
+        echo '<br/>';
+
+        $str = 'abcde';
+        echo $str[strlen($str) - 3];
+        echo '<br/>';
+
+        echo 'Zad 7';
+        echo '<br/>';
+
+        $str = '12345';
+        $sum = 0;
+         for ($i = 0; $i < strlen($str); $i++)
+            $sum += $str[$i];
+        echo $sum;
+        echo '<br/>';
+
+        echo 'Zad 8';
+        echo '<br/>';
+
+        $num = 12345;
+        $str = (string) $num;
+        $sum = 0;
+        for ($i = 0; $i < strlen($str); $i++)
+            $sum += $str[$i];
+        echo $sum;
+        echo '<br/>';
+
+        echo 'Zad 9';
+        echo '<br/>';
+
+        $num = 12345;
+        $str = (string) $num;
+        $sum = 1;
+        for ($i = 0; $i < strlen($str); $i++)
+            $sum *= $str[$i];
+        echo $sum;
+        echo '<br/>';
+
+        echo 'Zad 10';
+        echo '<br/>';
+
+        $num = 12345;
+        $str = (string) $num;
+        $sum = $str[4] . $str[3] . $str[2] . $str[1] . $str[0];
+        echo $sum;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+        echo 'Razdel 18';
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $num = 47;
+        $num += 7;
+        $num -= 18;
+        $num *= 10;
+        $num /= 15;
+        echo $num;
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $str = 'a';
+        $str .= 'b';
+        $str .= 'c';
+        echo $str;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 20';
+        echo '<br/>';
+
+        echo 'Zad 1';
+        echo '<br/>';
+
+        echo 24 * 60 * 60;
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        echo 30 * 24 * 60 * 60;
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        echo 365 * 24 * 60 * 60;
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        echo 24 * 60;
+        echo '<br/>';
+
+        echo 'Zad 5';
+        echo '<br/>';
+
+        echo 365 * 24 * 60;
+        echo '<br/>';
+
+        echo 'Zad 6';
+        echo '<br/>';
+
+        echo 1024 * 1024;
+        echo '<br/>';
+
+        echo 'Zad 7';
+        echo '<br/>';
+
+        echo 1024 * 1024 * 1024;
+        echo '<br/>';
+
+        echo 'Zad 8';
+        echo '<br/>';
+
+        echo 10 * 1024 * 1024 * 1024;
+        echo '<br/>';
+
+        echo 'Zad 9';
+        echo '<br/>';
+
+        echo 1024 * 1024 * 1024 * 1024;
+        echo '<br/>';
+
+        echo 'Zad 10';
+        echo '<br/>';
+
+        echo 1024 * 1024 * 1024;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 21';
+        echo '<br/>';
+
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $r = 5;
+        $s = 3.14 * $r ** 2;
+        echo $s;
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $a = 2;
+        $s = $a ** 2;
+        echo $s;
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $a = 2;
+        $b = 5;
+        $s = $a * $b;
+        echo $s;
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $a = 2;
+        $b = 5;
+        $s = $a * 2 + $b * 2;
+        echo $s;
+        echo '<br/>';
+
+        echo 'Zad 5';
+        echo '<br/>';
+
+        $tc = 10;
+        $tf = $tc * 1.8 + 32;
+        echo $tf;
+        echo '<br/>';
+
+        echo 'Zad 6';
+        echo '<br/>';
+
+        $tf = 50;
+        $tc = ($tf - 32) / 1.8;
+        echo $tc;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 22';
+        echo '<br/>';
+
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $a = ['a', 'b', 'c'];
+        echo $a;
+        echo '<br/>';
+        var_dump($a);
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $a = ['a', 'b', 'c'];
+        echo $a[0] . ' ' . $a[1] . ' ' . $a[2];
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $a = ['a', 'b', 'c', 'd'];
+        echo $a[0] . '+' . $a[1] . '+' . $a[2] . '+' . $a[3];
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $a = [1, 2, 3];
+        echo $a[0] + $a[1] + $a[2];
+        echo '<br/>';
+
+        echo 'Zad 5';
+        echo '<br/>';
+
+        $a = [2, 5, 3, 9];
+        $result = $a[0] * $a[1] + $a[2] * $a[3];
+        echo $result;
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 23';
+        echo '<br/>';
+
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $a = [1 => 'a', 2 => 'b', 3 => 'c'];
+        echo $a[1] . ' ' . $a[2] . ' ' . $a[3];
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $a = [1 => 'январь', 'февраль', 'март', 'апрель', 'май', 
+        'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $user = ['name' => 'Jora', 'surname' => 'Bob', 'patronymic' => 'BAKULA'];
+        echo $user['surname'] . ' ' . $user['name'] . ' ' . $user['patronymic'];
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $date = ['year' => 2023, 'month' => 'апрель', 'day' => 9];
+        echo $date['year'] . '-' . $date['month'] . '-' . $date['day'];
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 24';
+        echo '<br/>';
+
+        $arr = [3 => 'value3', 1 => 'value1', 2 => 'value2'];
+        echo $arr[1] . ' ' . $arr[2] . ' ' . $arr[3];
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 25';
+        echo '<br/>';
+
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $arr = ['a', 'b', 'c', 'd', 'e'];
+        echo count($arr);
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $arr = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4]; 
+        echo count($arr);
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $arr = ['a', 'b', 'c', 'd', 'e'];
+        echo $arr[count($arr) - 1];
+        echo '<br/>';
+
+        echo 'Zad 4';
+        echo '<br/>';
+
+        $arr = ['a', 'b', 'c', 'd', 'e'];
+        echo $arr[count($arr) - 2];
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 26';
+        echo '<br/>';
+
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $arr = ['a' => 1, 'b' => 2, 'c' => 3];
+        $arr['a'] = '!';
+        $arr['b'] = '5';
+        $arr['c'] = '-';
+
+        var_dump($arr);
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $arr = ['a' => 1, 'b' => 2, 'c' => 3];
+        $arr['a'] += 3;
+        $arr['b'] += 3;
+        $arr['c'] += 3;
+
+        var_dump($arr);
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 27';
+        echo '<br/>';
+
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $arr = [];
+        $arr[] = 1;
+        $arr[] = 2;
+        $arr[] = 3;
+        $arr[] = 4;
+        $arr[] = 5;
+
+        var_dump($arr);
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $arr = [1, 2, 3];
+        $arr[] = 4;
+        $arr[] = 5;
+
+        var_dump($arr);
+        echo '<br/>';
+
+        echo 'Zad 3';
+        echo '<br/>';
+
+        $arr = [];
+        $arr['year'] = 2023;
+        $arr['month'] = 4;
+        $arr['day'] = 9;
+
+        var_dump($arr);
+        echo '<br/>';
+        echo '<br/>';
+
+
+
+
+
+
+
+
+
+
+
+
+        echo 'Razdel 28';
+        echo '<br/>';
+
+        echo 'Zad 1';
+        echo '<br/>';
+
+        $arr = [1, 2, 3, 4, 5];
+        $key1 = 1;
+        $key2 = 2;
+
+        echo $arr[$key1] + $arr[$key2];
+        echo '<br/>';
+
+        echo 'Zad 2';
+        echo '<br/>';
+
+        $arr = ['a' => 1, 'b' => 2, 'c' => 3];
+        $key = 'b';
+        echo $arr[$key];
+
+
+    ?>
+</body>
+</html>
